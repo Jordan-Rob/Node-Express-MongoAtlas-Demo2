3e2eec31d0d521d570af84f04b7112d82b80bdfb
jordanrob709 <jordanrob709@gmail.com>
2020-07-02 23:59:02 +0300

    added schema

--- api/coolapi.js
+++ api/coolapi.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const coolioObj = require("../models/CoolModel");
 
 router.post("/signup", (request, response) => {
-  response.send("test");
+  const coolPerson = new coolioObj({
+    username: request.body.username,
+    email: request.body.email,
+    password: request.body.password,
+  });
+  coolPerson
+    .save()
+    .then((data) => {
+      response.json(data);
+    })
+    .catch((error) => {
+      response.json(error);
+    });
 });
 
 module.exports = router;
